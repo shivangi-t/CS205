6ba36a014e76961f2766b5634e8a577853da7556
Vinayak Agarwal <vinayakagarwal.dun@gmail.com>
2016-12-04 01:54:47 +0530

    Update models.py

--- Project/yellowline/theyellowline/models.py
+++ Project/yellowline/theyellowline/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 
-
+# Create your models here.
 class Event(models.Model):
-    event_name = models.CharField(max_length=200)
-    event_datetime = models.DateTimeField('date and time of event')
-    event_time = models.DateTimeField('date published')
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.event_name
+    event_name=models.CharField(max_length=200)
+    event_date=models.DateTimeField('event date')
+    
