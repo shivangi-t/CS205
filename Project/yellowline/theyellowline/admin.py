bb990407f77c8fe0a535797e76af90157324f926
Vinayak Agarwal <vinayakagarwal.dun@gmail.com>
2016-12-05 03:06:45 +0530

    Update admin.py

--- Project/yellowline/theyellowline/admin.py
+++ Project/yellowline/theyellowline/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Event
+from .models import Event,PaidEvent
 
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['event_name']}),
+        ('Date information', {'fields': ['event_date']}),
+        ('Venue',{'fields':['event_venue']}),
+        ('Category',{'fields':['event_category']}),
+        ('Webpage',{'fields':['event_website']}),
+        ('Paid Event',{'fields':['event_paid'], 'classes': ['collapse']})
+        
+    ]
+
+
+admin.site.register(Event,EventAdmin)
+admin.site.register(PaidEvent)
