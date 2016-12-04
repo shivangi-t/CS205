a1aa871e8f5c9d9e79dc664db6a3b44408231073
Vinayak Agarwal <vinayakagarwal.dun@gmail.com>
2016-12-05 03:11:09 +0530

    Update settings.py

--- Project/yellowline/yellowline/settings.py
+++ Project/yellowline/yellowline/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'yellow_line.apps.YellowLineConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+5:30'
 
 USE_I18N = True
 
